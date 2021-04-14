f19ff2aa58cc9196178b848280eb8414e1a7d502
Nepumi-Jr <peerator@kkumail.com>
2021-04-15 01:17:58 +0700

    ⚡️Remove duplicate Lang
    from Moneko's suggestion

--- BGAnimations/ScreenGameplay overlay/MoreINFO/default.lua
+++ BGAnimations/ScreenGameplay overlay/MoreINFO/default.lua
@@ -1,7 +1,18 @@
 local N = Def.ActorFrame{
 	
 	LoadActor("Jud")..{
-		OnCommand=cmd(x,SCREEN_CENTER_X-220;y,SCREEN_CENTER_Y+50);
+		OnCommand=function(self)
+            if GAMESTATE:IsPlayerEnabled(PLAYER_1) and Center1Player() then
+                self:x(SCREEN_CENTER_X-220);
+            elseif GAMESTATE:IsPlayerEnabled(PLAYER_2) and Center1Player() then
+                self:x(SCREEN_CENTER_X+220);
+            else
+                self:CenterX()
+            end
+            
+            self:y(SCREEN_CENTER_Y+50)
+
+        end;
 	};
 	LoadActor("OffsetBar")..{
 		OnCommand=cmd(CenterX;y,SCREEN_CENTER_Y*2-30);
