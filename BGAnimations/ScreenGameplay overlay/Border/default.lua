869dd9b8ff62490e4c6b17e78ec4c5139923253a
Nepumi-Jr <peerator@kkumail.com>
2022-06-23 20:05:31 +0700

    :bug: Border Failed player

--- BGAnimations/ScreenGameplay overlay/Border/default.lua
+++ BGAnimations/ScreenGameplay overlay/Border/default.lua
@@ -4,6 +4,7 @@ return { CD[1], CD[2], CD[3], CD[4]*A }
 end;
 local LP={0,0};
 local DP={0,0};
+local isDead = {false,false}
 local t = Def.ActorFrame{
 	OnCommand=function(self) self:y(-10); end;
 };
@@ -105,7 +106,10 @@ t[#t+1]=LoadActor("Border.png")..{
 		OnCommand=function(self) self:queuecommand("Judgment"); end;
 		JudgmentMessageCommand=function(self)
 		if SCREENMAN:GetTopScreen():GetLifeMeter(PLAYER_1):IsFailing() then
-			self:playcommand("Fail1")
+			if not isDead[1] then
+				isDead[1] = true
+				self:diffuse(PlayerColor(PLAYER_1)):decelerate(0.5):diffuse(color("#00000000"));
+			end
 		else
 			if GAMESTATE:IsPlayerEnabled(PLAYER_1) and SCREENMAN:GetTopScreen():GetLifeMeter(PLAYER_1) then
 					DP[1] = 50-(math.min(SCREENMAN:GetTopScreen():GetLifeMeter(PLAYER_1):GetLife()*100,50));
@@ -114,16 +118,9 @@ t[#t+1]=LoadActor("Border.png")..{
 			if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
 				self:cropright(0.5)
 			end
-			if GAMESTATE:GetPlayerState(PLAYER_1):GetCurrentPlayerOptions():DrainSetting() == "DrainType_SuddenDeath" then
-				self:diffuseshift():effectcolor1({1,0,0,1}):effectcolor2({1,0,0,0.5}):effectclock("beat");
-				--self:diffusealpha((1-(math.mod(GAMESTATE:GetSongBeat(),1)))/2)
-			else
-				self:stopeffect();
-				self:diffusealpha(DP[1]/50)
-			end
+			self:diffusealpha(DP[1]/50)
 		end
 		end;
-		Fail1Command=function(self) self:diffuse(PlayerColor(PLAYER_1)); self:decelerate(0.5); self:diffuse(color("#00000000")); end;
 		GETOUTOFGAMESMMessageCommand=function(self) self:stoptweening(); self:sleep(0.75); self:accelerate(1); self:diffusealpha(0); end;
 		};
 end
@@ -134,7 +131,10 @@ t[#t+1]=LoadActor("Border.png")..{
 		OnCommand=function(self) self:queuecommand("Judgment"); end;
 		JudgmentMessageCommand=function(self)
 		if SCREENMAN:GetTopScreen():GetLifeMeter(PLAYER_2):IsFailing() then
-			self:playcommand("Fail2")
+			if not isDead[2] then
+				isDead[2] = true
+				self:diffuse(PlayerColor(PLAYER_2)):decelerate(0.5):diffuse(color("#00000000"));
+			end
 		else
 			if GAMESTATE:IsPlayerEnabled(PLAYER_2) and SCREENMAN:GetTopScreen():GetLifeMeter(PLAYER_2) then
 					DP[2] = 50-(math.min(SCREENMAN:GetTopScreen():GetLifeMeter(PLAYER_2):GetLife()*100,50));
@@ -143,13 +143,7 @@ t[#t+1]=LoadActor("Border.png")..{
 			if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
 				self:cropleft(0.5)
 			end
-			if GAMESTATE:GetPlayerState(PLAYER_2):GetCurrentPlayerOptions():DrainSetting() == "DrainType_SuddenDeath" then
-				self:diffuseshift():effectcolor1({1,0,0,1}):effectcolor2({1,0,0,0.5}):effectclock("beat");
-				--self:diffusealpha((1-(math.mod(GAMESTATE:GetSongBeat(),1)))/2)
-			else
-				self:stopeffect();
-				self:diffusealpha(DP[2]/50)
-			end
+			self:diffusealpha(DP[2]/50)
 		end
 		end;
 		Fail2Command=function(self) self:diffuse(PlayerColor(PLAYER_2)); self:decelerate(0.5); self:diffuse(color("#00000000")); end;
