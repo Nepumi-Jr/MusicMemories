60383d60595593fedd6588cf00a7c4c7e9a3967b
Nepumi-Jr <peerator@kkumail.com>
2022-06-23 21:17:26 +0700

    :bug: Tap offset

--- Modules/SubJudgment.TapOffset.lua
+++ Modules/SubJudgment.TapOffset.lua
@@ -6,22 +6,12 @@ return function(player)
 
 				if param.Player ~= player then return end;
 				if param.HoldNoteScore then return end;
-
-				if param.TapNoteScore=="TapNoteScore_W5" or
-				param.TapNoteScore=="TapNoteScore_W4" or
-				param.TapNoteScore=="TapNoteScore_W3" or
-				param.TapNoteScore=="TapNoteScore_W2" or
-				param.TapNoteScore=="TapNoteScore_W1" or
-                param.TapNoteScore=="TapNoteScore_ProW1" or
-                param.TapNoteScore=="TapNoteScore_ProW2" or
-                param.TapNoteScore=="TapNoteScore_ProW3" or
-                param.TapNoteScore=="TapNoteScore_ProW4" or
-                param.TapNoteScore=="TapNoteScore_ProW5" then
+                local TNSList = {"TapNoteScore_W5","TapNoteScore_W4","TapNoteScore_W3","TapNoteScore_W2","TapNoteScore_W1","TapNoteScore_ProW1","TapNoteScore_ProW2","TapNoteScore_ProW3","TapNoteScore_ProW4","TapNoteScore_ProW5"}
+                
+				if FindInTable(param.TapNoteScore, TNSList) ~= nil then
                     self:stoptweening():settextf(" ms")
                     self:visible(true)
                     self:diffuse((param.Early) and color("#00B4FF") or color("#FF0096")):diffusealpha(1):zoom(1.05):decelerate(0.125):zoom(1):sleep(0.8):decelerate(0.1):diffusealpha(0)
-				else
-                    self:visible(false):stoptweening()
                 end
 			end;
         };
@@ -32,21 +22,12 @@ return function(player)
 				if param.Player ~= player then return end;
 				if param.HoldNoteScore then return end;
 
-				if param.TapNoteScore=="TapNoteScore_W5" or
-				param.TapNoteScore=="TapNoteScore_W4" or
-				param.TapNoteScore=="TapNoteScore_W3" or
-				param.TapNoteScore=="TapNoteScore_W2" or
-				param.TapNoteScore=="TapNoteScore_W1" or
-                param.TapNoteScore=="TapNoteScore_ProW1" or
-                param.TapNoteScore=="TapNoteScore_ProW2" or
-                param.TapNoteScore=="TapNoteScore_ProW3" or
-                param.TapNoteScore=="TapNoteScore_ProW4" or
-                param.TapNoteScore=="TapNoteScore_ProW5" then
+				local TNSList = {"TapNoteScore_W5","TapNoteScore_W4","TapNoteScore_W3","TapNoteScore_W2","TapNoteScore_W1","TapNoteScore_ProW1","TapNoteScore_ProW2","TapNoteScore_ProW3","TapNoteScore_ProW4","TapNoteScore_ProW5"}
+                
+				if FindInTable(param.TapNoteScore, TNSList) ~= nil then
                     self:stoptweening():settextf("%d", param.TapNoteOffset * 1000 or 1000)
                     self:visible(true)
                     self:diffuse((param.Early) and color("#00B4FF") or color("#FF0096")):diffusealpha(1):zoom(1.05):decelerate(0.125):zoom(1):sleep(0.8):decelerate(0.1):diffusealpha(0)
-				else
-                    self:visible(false):stoptweening()
                 end
 			end;
         };
