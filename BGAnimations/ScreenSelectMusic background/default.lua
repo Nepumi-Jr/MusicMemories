d9e9586ac5138f95eee4509fa6c9aa269a9b07b0
nepumi <peerator@kkumail.com>
2023-09-11 00:24:38 +0700

    Change screen music background

--- BGAnimations/ScreenSelectMusic background/default.lua
+++ BGAnimations/ScreenSelectMusic background/default.lua
@@ -2,10 +2,9 @@ local t = Def.ActorFrame{
 
 	LoadActor(THEME:GetPathG("Global","Background"));
 	Def.ActorFrame{
-		OnCommand=function(self) self:effectclock("beat"); self:diffuseramp(); self:effectcolor1(color("1,1,1,0.9")); self:effectcolor2(color("1,1,1,0.7")); self:effecttiming(.5,.5,0,0); end;
+		InitCommand=function(self) self:diffusealpha(1); self:bob(); self:effectclock('beat'); self:effectmagnitude(1,3,0):zoom(1.1); self:effectperiod(32); end;
 		Def.Sprite {
-			InitCommand=function(self) self:diffusealpha(1); self:pulse(); self:effectclock('beat'); self:effectmagnitude(1.025,1,1); self:effecttiming(1,0,1,0); end;
-			CurrentSongChangedMessageCommand=function(self) self:x(SCREEN_CENTER_X); self:y(SCREEN_CENTER_Y); self:finishtweening(); self:sleep(0.32); self:queuecommand("ModifySongBackground"); end;
+			CurrentSongChangedMessageCommand=function(self) self:finishtweening():sleep(0.3) self:queuecommand("ModifySongBackground"); end;
 			ModifySongBackgroundCommand=function(self)
 				local cur_song = GAMESTATE:GetCurrentSong()
 				if cur_song then
@@ -17,7 +16,26 @@ local t = Def.ActorFrame{
 						self:visible(false);
 						self:Load(nil)
 					end;
-					self:diffusealpha(scale(LoadModule("ColorTone.Nighty.lua")(),0,1,1,0.75))
+				else
+					self:visible(false);
+					self:Load(nil)
+				end
+			end;	
+		};
+		Def.Sprite {
+			CurrentSongChangedMessageCommand=function(self) self:finishtweening():croptop(1) self:queuecommand("ModifySongBackground"); end;
+			ModifySongBackgroundCommand=function(self)
+				local cur_song = GAMESTATE:GetCurrentSong()
+				if cur_song then
+					if cur_song:GetBackgroundPath() then
+						self:visible(true);
+						self:LoadBackground(cur_song:GetBackgroundPath());
+						self:stretchto(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
+					else
+						self:visible(false);
+						self:Load(nil)
+					end;
+					self:croptop(1):fadetop(0.2):decelerate(0.3):croptop(0):fadetop(0)
 				else
 					self:visible(false);
 					self:Load(nil)
@@ -25,34 +43,6 @@ local t = Def.ActorFrame{
 			end;	
 		};
 	};
-
-	Def.Sprite {
-		InitCommand=function(self) self:x(0); self:y(0); self:diffusealpha(0.8); end;
-		CurrentSongChangedMessageCommand=function(self) self:finishtweening(); self:sleep(0.32); self:queuecommand("ModifySongBackground"); end;
-		ModifySongBackgroundCommand=function(self)
-			if GAMESTATE:GetCurrentSong() then
-				if GAMESTATE:GetCurrentSong():GetPreviewVidPath() then
-					
-					self:visible(true);
-					self:Load(GAMESTATE:GetCurrentSong():GetPreviewVidPath());
-					self:stretchto(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
-					self:diffusealpha(0)
-					self:sleep(0.5)
-					self:accelerate(1):diffusealpha(scale(LoadModule("ColorTone.Nighty.lua")(),0,1,0.55,0.8))
-				else
-					self:Load(nil)
-					self:visible(false);
-				end;
-			else
-				self:Load(nil)
-				self:visible(false);
-			end;
-		end;	
-	};
-
-
-
-	
 };
 
 
